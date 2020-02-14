5070c2ed13a1bca9c3c03a851c8636fb221ebfb5
Jimmy Xu <jimmyxu@users.noreply.github.com>
2020-02-14 00:44:34 +0000

    Download stuffs ourselves

--- epub.sh
+++ epub.sh
@@ -1,18 +1,22 @@
 #!/opt/local/bin/bash -
 
-if [ ! -f rsa.pem ]; then
-    sqlite3 ~/Library/Application\ Support/Readmoo/Local\ Storage/app_readmoo_0.localstorage 'SELECT value FROM itemtable WHERE key = "rsa_privateKey";' >rsa.pem
+shopt -s nullglob
+
+cd "$(dirname "$0")"
+if [ ! -f rsa.key ]; then
+    exit 1
 fi
 
 mkdir -p books
 cd books
 
-for bookzip in ~/Library/Containers/com.readmoo.readmoodesktop/Readmoo/api/book/*; do
+for bookzip in *.zip; do
     book="$(basename "$bookzip" .zip)"
 
     if [ ! -d "$book" ]; then
         mkdir -p "$book"
         unzip "$bookzip" -d "$book"
+        rm "$bookzip"
     fi
 
     if [ ! -f "$book"/META-INF/encryption.xml ]; then
@@ -20,15 +24,16 @@ for bookzip in ~/Library/Containers/com.readmoo.readmoodesktop/Readmoo/api/book/
     fi
     pushd "$book"
 
-    ek="$(xmllint META-INF/encryption.xml --xpath '//*[local-name()="CipherValue"]/text()' | base64 -d | openssl rsautl -decrypt -inkey ../../rsa.pem | xxd -p)"
+    ek="$(xmllint META-INF/encryption.xml --xpath '//*[local-name()="CipherValue"]/text()' | base64 -d | openssl rsautl -decrypt -inkey ../../rsa.key | xxd -p)"
 
     for file in $(xmllint META-INF/encryption.xml --xpath '//*[local-name()="CipherReference"]/@URI' | sed -E -e 's/ +URI="([^"]+)"/\1 /g'); do
+        echo ... "$file"
         iv="$(head -c16 "$file" | xxd -p)"
         tail -c+17 "$file" | openssl enc -aes128 -d -K "$ek" -iv "$iv" -out "$file".plain
         mv "$file".plain "$file"
     done
 
     rm META-INF/encryption.xml
-    zip -r ../"$book".epub *
+    zip -9 -r ../"$book".epub *
     popd
 done
---
+++ readmoo.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import subprocess
+import uuid
+import tqdm
+
+CLIENT_ID = ''
+TOKEN = ''
+DIR = os.path.expanduser('~/src/readmoo/')
+UDID = None
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
+PUBKEY = None
+
+def init_udid():
+    global UDID
+    try:
+        with open(DIR + 'udid.txt') as f:
+            UDID = f.read().strip()
+    except IOError:
+        pass
+    if not UDID:
+        UDID = str(uuid.uuid4())
+        with open(DIR + 'udid.txt', 'w') as f:
+            f.write(UDID)
+
+def init_pubkey():
+    global PUBKEY
+    ret = False
+    if not os.path.exists(DIR + 'rsa.pub'):
+        subprocess.run(['openssl', 'genrsa', '-out', DIR + 'rsa.key', '1024'], check=True)
+        subprocess.run(['openssl', 'rsa', '-in', DIR + 'rsa.key', '-pubout', '-out', DIR + 'rsa.pub'], check=True)
+        ret = True
+    with open(DIR + 'rsa.pub') as f:
+        PUBKEY = f.read()
+    return ret
+
+def main():
+    init_udid()
+    pubkey = init_pubkey()
+    if not TOKEN:
+        oauthURL = f'https://member.readmoo.com/oauth?client_id={CLIENT_ID}&redirect_uri=app://readmoo/login.html&udid={UDID}&response_type=token&scope=reading,highlight,like,comment,me,library&custom_layout=desktop'
+        print(oauthURL)
+        return
+    s = requests.Session()
+    s.headers.update({'User-Agent': USER_AGENT, 'Authorization': f'Client {CLIENT_ID}'})
+    me = s.get(f'https://api.readmoo.com/me?access_token={TOKEN}').json()
+    user_id = me['user']['id']
+    tags = s.get(f'https://api.readmoo.com/me/tags?access_token={TOKEN}').json()
+    books = tags['items'][0]['tag']['books']
+
+    if pubkey:
+        s.post(f'https://api.readmoo.com/me/devices/{UDID}/publickey',
+                params={'access_token': TOKEN, 'client_id': CLIENT_ID},
+                data={'KeyName': user_id, 'KeyValue': PUBKEY})
+
+    ls = os.listdir(DIR + 'books')
+    for book in books:
+        if book in ls or f'{book}.zip' in ls:
+            continue
+        url = f'https://api.readmoo.com/epub/{book}?client_id={CLIENT_ID}&access_token={TOKEN}'
+        print(url)
+        r = s.get(url, stream=True)
+        with tqdm.tqdm(total=int(r.headers.get('content-length', 0)), unit='iB', unit_scale=True, unit_divisor=1024) as t, open(DIR + f'books/{book}.zip', 'wb') as f:
+            for d in r.iter_content(1024):
+                t.update(len(d))
+                f.write(d)
+
+    subprocess.run(['bash', DIR + 'epub.sh'], check=True)
+
+
+if __name__ == '__main__':
+    main()
